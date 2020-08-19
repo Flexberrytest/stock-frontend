d81595d6b37b98a5368ecb9e4920288489295baf
Flexberrytest <emberplugin@mail.ru>
2020-08-19 05:47:24 +0000

    Flexberry code generation

---
+++ app/locales/ru/models/i-i-s-stock-склад.js
@@ -0,0 +1,15 @@
+export default {
+  projections: {
+  },
+  validations: {
+    адрес: {
+      __caption__: 'адрес'
+    },
+    работников: {
+      __caption__: 'работников'
+    },
+    товар: {
+      __caption__: 'товар'
+    }
+  }
+};
---
+++ app/models/custom-inflector-rules.js
@@ -0,0 +1,8 @@
+import Inflector from 'ember-inflector';
+
+const inflector = Inflector.inflector;
+
+inflector.irregular('склад', 'Складs');
+inflector.irregular('товар', 'Товарs');
+
+export default {};
