01e23e8071df7361374dd6ac4ac40c7eea32a00a
Flexberrytest <emberplugin@mail.ru>
2020-08-19 05:48:02 +0000

    Flexberry code generation

--- app/models/custom-inflector-rules.js
+++
@@ -1,8 +0,0 @@
-import Inflector from 'ember-inflector';
-
-const inflector = Inflector.inflector;
-
-inflector.irregular('склад', 'Складs');
-inflector.irregular('товар', 'Товарs');
-
-export default {};
---
+++ assets/tests.js
@@ -0,0 +1,332 @@
+'use strict';
+
+define('ember-app/tests/app.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | app');
+
+  QUnit.test('adapters/application-offline.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'adapters/application-offline.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('adapters/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'adapters/application.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('app.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'app.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('controllers/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'controllers/application.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('locales/en/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'locales/en/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('locales/en/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'locales/en/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('locales/en/translations.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'locales/en/translations.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('locales/ru/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'locales/ru/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('locales/ru/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'locales/ru/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('locales/ru/translations.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'locales/ru/translations.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('mixins/regenerated/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mixins/regenerated/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('mixins/regenerated/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mixins/regenerated/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('mixins/regenerated/serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-offline.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mixins/regenerated/serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-offline.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('mixins/regenerated/serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mixins/regenerated/serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('mixins/regenerated/serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-offline.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mixins/regenerated/serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-offline.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('mixins/regenerated/serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'mixins/regenerated/serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('models/custom-inflector-rules.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/custom-inflector-rules.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('resolver.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'resolver.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('router.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'router.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/application.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('routes/index.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'routes/index.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('serializers/application-offline.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'serializers/application-offline.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('serializers/application.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'serializers/application.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-offline.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-offline.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-offline.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-offline.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('services/offline-globals.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'services/offline-globals.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('services/store.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'services/store.js should pass ESLint\n\n');
+  });
+});
+define('ember-app/tests/helpers/ember-i18n/test-helpers', ['ember-i18n/test-support/-private/t', 'ember-i18n/test-support/-private/assert-translation'], function (_t2, _assertTranslation2) {
+  'use strict';
+
+  // example usage: find(`.header:contains(${t('welcome_message')})`)
+  Ember.Test.registerHelper('t', function (app, key, interpolations) {
+    return (0, _t2.default)(app.__container__, key, interpolations);
+  });
+
+  // example usage: expectTranslation('.header', 'welcome_message');
+  Ember.Test.registerHelper('expectTranslation', function (app, element, key, interpolations) {
+    const text = (0, _t2.default)(app.__container__, key, interpolations);
+
+    (0, _assertTranslation2.default)(element, key, text);
+  });
+});
+define('ember-app/tests/helpers/ember-prop-types', ['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.createComponent = createComponent;
+
+  const { VERSION } = Ember;
+
+  /**
+   * Determine if we are on a version of Ember that includes Glimmer 2
+   * @returns {Boolean} whether or not we are on Glimmer 2
+   */
+  function isGlimmer2() {
+    const [major, minor] = VERSION.split('.');
+    return parseInt(major) > 1 && parseInt(minor) > 9;
+  }
+
+  /**
+   * Programitcally instantiate instance of component class
+   * @param {Ember.Component} component - component class to instantiate
+   * @returns {Ember.Component} instance of component class
+   */
+  function createComponent(component) {
+    if (isGlimmer2()) {
+      return component.create({ renderer: {} });
+    }
+
+    return component.create();
+  }
+});
+define('ember-app/tests/test-helper', ['ember-app/app', 'ember-app/config/environment', '@ember/test-helpers', 'ember-qunit'], function (_app, _environment, _testHelpers, _emberQunit) {
+  'use strict';
+
+  (0, _testHelpers.setApplication)(_app.default.create(_environment.default.APP));
+
+  (0, _emberQunit.start)();
+});
+define('ember-app/tests/tests.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | tests');
+
+  QUnit.test('test-helper.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'test-helper.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-test.js should pass ESLint\n\n');
+  });
+
+  QUnit.test('unit/serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-test.js', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-test.js should pass ESLint\n\n');
+  });
+});
+define('ember-app/tests/unit/models/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForModel)('i-i-s-stock-склад', 'Unit | Model | i-i-s-stock-склад', {
+    // Specify the other units that are required for this test.
+    needs: ['model:i-i-s-stock-склад', 'model:i-i-s-stock-товар', 'validator:ds-error', 'validator:presence', 'validator:number', 'validator:date', 'validator:belongs-to', 'validator:has-many', 'service:syncer']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    let model = this.subject();
+
+    // let store = this.store();
+    assert.ok(!!model);
+  });
+});
+define('ember-app/tests/unit/models/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForModel)('i-i-s-stock-товар', 'Unit | Model | i-i-s-stock-товар', {
+    // Specify the other units that are required for this test.
+    needs: ['model:i-i-s-stock-склад', 'model:i-i-s-stock-товар', 'validator:ds-error', 'validator:presence', 'validator:number', 'validator:date', 'validator:belongs-to', 'validator:has-many', 'service:syncer']
+  });
+
+  (0, _emberQunit.test)('it exists', function (assert) {
+    let model = this.subject();
+
+    // let store = this.store();
+    assert.ok(!!model);
+  });
+});
+define('ember-app/tests/unit/serializers/i-i-s-stock-\u0441\u043A\u043B\u0430\u0434-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForModel)('i-i-s-stock-склад', 'Unit | Serializer | i-i-s-stock-склад', {
+    // Specify the other units that are required for this test.
+    needs: ['serializer:i-i-s-stock-склад', 'service:syncer', 'transform:file', 'transform:decimal', 'transform:guid', 'model:i-i-s-stock-склад', 'model:i-i-s-stock-товар', 'validator:ds-error', 'validator:presence', 'validator:number', 'validator:date', 'validator:belongs-to', 'validator:has-many']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it serializes records', function (assert) {
+    let record = this.subject();
+
+    let serializedRecord = record.serialize();
+
+    assert.ok(serializedRecord);
+  });
+});
+define('ember-app/tests/unit/serializers/i-i-s-stock-\u0442\u043E\u0432\u0430\u0440-test', ['ember-qunit'], function (_emberQunit) {
+  'use strict';
+
+  (0, _emberQunit.moduleForModel)('i-i-s-stock-товар', 'Unit | Serializer | i-i-s-stock-товар', {
+    // Specify the other units that are required for this test.
+    needs: ['serializer:i-i-s-stock-товар', 'service:syncer', 'transform:file', 'transform:decimal', 'transform:guid', 'model:i-i-s-stock-склад', 'model:i-i-s-stock-товар', 'validator:ds-error', 'validator:presence', 'validator:number', 'validator:date', 'validator:belongs-to', 'validator:has-many']
+  });
+
+  // Replace this with your real tests.
+  (0, _emberQunit.test)('it serializes records', function (assert) {
+    let record = this.subject();
+
+    let serializedRecord = record.serialize();
+
+    assert.ok(serializedRecord);
+  });
+});
+define('ember-app/config/environment', [], function() {
+  var prefix = 'ember-app';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+require('ember-app/tests/test-helper');
+EmberENV.TESTS_FILE_LOADED = true;
+//# sourceMappingURL=tests.map
